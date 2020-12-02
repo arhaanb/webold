4d1ac7be8d14c0ec63eb21f1de674470b392af2f
arhaanb <arhaanb@gmail.com>
2020-12-02 19:17:18 +0530

    set status

--- api/redirect.ts
+++ api/redirect.ts
@@ -48,7 +48,8 @@ export default async (req: NowRequest, res: NowResponse) => {
 
   console.error(`[${timestamp}] ${ip} -> ${source} -> n/a`)
   // res.status(404).json({ error: 'link not found', source, timestamp });
-  res.status(404).redirect('/404');
+  res.status(200).redirect('/404')
+  // res.sendFile('views/test.html', {root: __dirname })
 }
 
 interface Link {
