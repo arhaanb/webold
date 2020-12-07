4e4aaaba681747a742310545f670fa16455e8af5
arhaanb <arhaanb@gmail.com>
2020-12-07 16:33:10 +0530

    djsksn

--- assets/main.js
+++ assets/main.js
@@ -39,8 +39,8 @@ albumArt.setAttribute("src", defAlbum)
 function getSong() {
 	var client = new HttpClient();
 	// for development (TODO: Set up `vercel dev`)
-	client.get('https://cors-anywhere.herokuapp.com/https://arhaanb.co/api/spotify', function (response) {
-		// client.get('/api/spotify', function (response) {
+	// client.get('https://cors-anywhere.herokuapp.com/https://arhaanb.co/api/spotify', function (response) {
+	client.get('/api/spotify', function (response) {
 		var song = JSON.parse(response);
 		//console.log(song)
 		if (song.isPlaying === true) {
