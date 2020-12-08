a34760d510d138afdf19a0c779c60edc3c470f13
arhaanb <arhaanb@gmail.com>
2020-12-08 15:52:28 +0530

    clean

--- assets/js/toptracks.js
+++ assets/js/toptracks.js
@@ -1,26 +1,11 @@
-// // Default Values
-// var songVal = "Not Playing"
-// var albumVal = "Spotify"
-// var defAlbum = './assets/img/default-min.png'
-
-var spotifyContainer = document.getElementById('spotify-con')
-var songTitle = document.getElementById('songname')
-var album = document.getElementById('album')
-// var songlink = document.getElementById('songlink')
-var albumArt = document.getElementById('albumart')
-var equalizer = document.getElementById('equalizer')
-
-function getSong() {
+function getSongs() {
 	var client = new HttpClient();
 	client.get('/api/top-tracks', function (response) {
 		var tracks = JSON.parse(response);
-		console.log(tracks.tracks)
 		var text = "";
 		var i;
 
 		for (i = 0; i < tracks.tracks.length; i++) {
-			// text += tracks.tracks[i].title + "<br>";
-
 			text += `
 			<div class="spotify" id="spotify-con">
 				<img id="albumart" src=${tracks.tracks[i].albumArt} alt="">
@@ -33,30 +18,7 @@ function getSong() {
 		}
 
 		document.getElementById('top-tracks').innerHTML = text
-		// if (song.isPlaying === true) {
-		// 	songTitle.innerText = song.title
-		// 	songTitle.setAttribute("href", song.songUrl)
-		// 	songTitle.setAttribute("target", "_blank")
-
-		// 	album.innerText = song.artist
-		// 	albumArt.setAttribute("src", song.albumImageUrl)
-		// 	equalizer.style.display = "block";
-		// 	spotifyContainer.style.alignItems = "center"
-		// } else {
-		// 	songTitle.innerText = 'Not Playing'
-		// 	songTitle.setAttribute("href", "#")
-		// 	songTitle.setAttribute("target", "_self")
-
-		// 	album.innerText = 'Spotify'
-		// 	albumArt.setAttribute("src", './assets/img/default-min.png')
-		// 	equalizer.style.display = "none";
-		// 	spotifyContainer.style.alignItems = "flex-start"
-		// }
 	});
 }
 
-// setInterval(function () {
-// 	getSong()
-// }, 20000)
-
-getSong()
+getSongs()
