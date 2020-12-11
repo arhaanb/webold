f49a514460802b2f19edf221f287fc083c9d809e
arhaanb <arhaanb@gmail.com>
2020-12-11 21:45:59 +0530

    fix

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -9,15 +9,6 @@ function darkMode() {
 	var img = document.getElementById('albumart')
 }
 
-function calculate_age(dob) {
-	var diff_ms = Date.now() - dob.getTime();
-	var age_dt = new Date(diff_ms);
-
-	return Math.abs(age_dt.getUTCFullYear() - 1970);
-}
-
-document.getElementById('age').innerText = calculate_age(new Date(2003, 07, 23));
-
 var HttpClient = function () {
 	this.get = function (aUrl, aCallback) {
 		var anHttpRequest = new XMLHttpRequest();
