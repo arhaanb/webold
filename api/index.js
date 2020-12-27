c89455050ec92b4fcc2bd6f089b9ef157bf13b4a
arhaanb <arhaanb@gmail.com>
2020-12-27 18:42:08 +0530

    fix gh shortn

--- api/index.js
+++ api/index.js
@@ -1,86 +1,85 @@
 const express = require('express')
 const fetch = require('node-fetch')
-var Airtable = require('airtable');
-var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+var Airtable = require('airtable')
+var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_ID,
+)
 
 fs = require('fs')
 const path = require('path')
 const util = require('util')
 const dirPath = path.join(__dirname, '/../404.html')
 
-const readFile = util.promisify(fs.readFile);
+const readFile = util.promisify(fs.readFile)
 
 function getErr() {
-	return readFile(dirPath, 'utf8');
+  return readFile(dirPath, 'utf8')
 }
 
 const app = express()
 
-app.get('/*', async (req, res) => {
-	const url = req.url.slice(1).toLowerCase()
-	// console.log(url)
-	base('Links').select({
-		maxRecords: 1,
-		filterByFormula: `resolvedUid = "${url}"`
-		// filterByFormula: `resolvedUid = "${req.params.shrtn}"`
-	}).eachPage(function page(records) {
+app.get('/gh/:repo', async (req, res) => {
+  try {
+    const repo = req.params.repo
+    const { ok: yuh } = await fetch(
+      `https://api.github.com/repos/arhaanb/${repo}`,
+    )
 
-		// console.log(records)
+    if (yuh) {
+      return res.redirect(302, `https://github.com/arhaanb/${repo}`)
+    }
 
-		if (records.length > 0) {
-			if (records[0].get('enabled') == 1) {
-				var redirectUri = records[0].get('url')
-				// return res.send(redirectUri)
-				return res.status(302).redirect(redirectUri)
-			}
-		}
-
-		getErr().then(data => {
-			return res.status(404).send(data)
-		})
-
-
-	}, function done(err) {
-		if (err) {
-			console.error(err);
-			getErr().then(data => {
-				return res.status(404).send(data)
-			})
-		}
-	});
+    getErr().then((data) => {
+      return res.status(404).send(data)
+    })
+  } catch (e) {
+    console.error(e)
+  }
 })
 
-app.get('/gh/:repo', async (req, res) => {
-	try {
-		const repo = req.params.repo
+app.get('/*', async (req, res) => {
+  const url = req.url.slice(1).toLowerCase()
+  // console.log(url)
+  base('Links')
+    .select({
+      maxRecords: 1,
+      filterByFormula: `resolvedUid = "${url}"`,
+      // filterByFormula: `resolvedUid = "${req.params.shrtn}"`
+    })
+    .eachPage(
+      function page(records) {
+        // console.log(records)
 
-		const { ok: yuh } = await fetch(
-			`https://api.github.com/repos/arhaanb/${repo}`
-		)
+        if (records.length > 0) {
+          if (records[0].get('enabled') == 1) {
+            var redirectUri = records[0].get('url')
+            // return res.send(redirectUri)
+            return res.status(302).redirect(redirectUri)
+          }
+        }
 
-		if (yuh) {
-			return res.redirect(302, `https://github.com/arhaanb/${repo}`)
-		}
-
-		getErr().then(data => {
-			return res.status(404).send(data)
-		})
-
-	} catch (e) {
-		console.error(e)
-	}
+        getErr().then((data) => {
+          return res.status(404).send(data)
+        })
+      },
+      function done(err) {
+        if (err) {
+          console.error(err)
+          getErr().then((data) => {
+            return res.status(404).send(data)
+          })
+        }
+      },
+    )
 })
 
 app.use(function (req, res, next) {
-
-	getErr().then(data => {
-		return res.status(404).send(data)
-	})
-
-});
-
+  getErr().then((data) => {
+    return res.status(404).send(data)
+  })
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, (err) => {
-	if (err) throw err
+  if (err) throw err
 })
