75d4f940e77e2d3d3db83ba16a945abe9dc69d6d
arhaanb <arhaanb@gmail.com>
2021-01-08 14:15:10 +0530

    set title for truncated song names

--- assets/js/nowplaying.js
+++ assets/js/nowplaying.js
@@ -11,33 +11,35 @@ var albumArt = document.getElementById('albumart')
 var equalizer = document.getElementById('equalizer')
 
 function getSong() {
-	var client = new HttpClient();
-	client.get('/api/spotify', function (response) {
-		var song = JSON.parse(response);
-		if (song.isPlaying === true) {
-			songTitle.innerText = song.title
-			songTitle.setAttribute("href", song.songUrl)
-			songTitle.setAttribute("target", "_blank")
+  var client = new HttpClient()
+  client.get('/api/spotify', function (response) {
+    var song = JSON.parse(response)
+    if (song.isPlaying === true) {
+      songTitle.innerText = song.title
+      songTitle.setAttribute('href', song.songUrl)
+      songTitle.setAttribute('target', '_blank')
+      songTitle.setAttribute('title', song.title)
 
-			album.innerText = song.artist
-			albumArt.setAttribute("src", song.albumImageUrl)
-			equalizer.style.display = "block";
-			spotifyContainer.style.alignItems = "center"
-		} else {
-			songTitle.innerText = 'Not Playing'
-			songTitle.setAttribute("href", "#")
-			songTitle.setAttribute("target", "_self")
+      album.innerText = song.artist
+      albumArt.setAttribute('src', song.albumImageUrl)
+      equalizer.style.display = 'block'
+      spotifyContainer.style.alignItems = 'center'
+    } else {
+      songTitle.innerText = 'Not Playing'
+      songTitle.setAttribute('href', '#')
+      songTitle.setAttribute('target', '_self')
+      songTitle.setAttribute('title', '')
 
-			album.innerText = 'Spotify'
-			albumArt.setAttribute("src", './assets/img/def_dark.png')
-			equalizer.style.display = "none";
-			spotifyContainer.style.alignItems = "flex-start"
-		}
-	});
+      album.innerText = 'Spotify'
+      albumArt.setAttribute('src', './assets/img/def_dark.png')
+      equalizer.style.display = 'none'
+      spotifyContainer.style.alignItems = 'flex-start'
+    }
+  })
 }
 
 setInterval(function () {
-	getSong()
+  getSong()
 }, 20000)
 
 getSong()
